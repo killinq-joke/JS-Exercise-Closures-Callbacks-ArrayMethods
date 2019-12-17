7c24cc7df574e4883ece54a888a020ef8280f294
Zak <zakadmaster@gmail.com>
2019-12-17 15:00:54 +0100

    Addition of tallyUpDonations functionalities

--- index.js
+++ index.js
@@ -239,8 +239,10 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  return runners.reduce((acc, currentItem, index, array) => {
+    return acc + currentItem.donation;
+  }, 0);
 }
 
 /////////////// CLOSURES ///////////////
